48b633f3343c4cd60ac989a998f1dafe2dd2136c
Joel Thoresson <joelt86@hotmail.com>
2016-05-04 22:18:53 +0200

    Initial commit. Spring boot with dependencies Web, Rest Repositories, HATEOAS, DevTools, MongoDB Acuator and Actuator Docs

---
+++ src/main/java/com/wooz86/SpringrestapiApplication.java
@@ -0,0 +1,12 @@
+package com.wooz86;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wooz86/SpringrestapiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.wooz86;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringrestapiApplication.class)
+@WebAppConfiguration
+public class SpringrestapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
